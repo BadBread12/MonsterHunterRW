b2e9a4062d7e2a91a0e13044ea0c847bf5902a23
BadBread12 <127753076+BadBread12@users.noreply.github.com>
2024-06-13 12:11:26 +0200

    Test für zweidimensionalen Array
    einfach das obere weapons löschen und die Kommentare wieder entfernen

--- script.js
+++ script.js
@@ -6,16 +6,33 @@ const emptyPNG = '/Empty.png';
 const weapons = [];
 const weaponNames = [];
 
-weapons.push('/GS_ICON.png', '/LS_ICON.png', '/SNS_ICON.png', '/DB_ICON.png', '/HA_ICON.png', '/HH_ICON.png','/LZ_ICON.png','/GLZ_ICON.png','/MA_ICON.png','/CB_ICON.png','/GLV_ICON.png','/LBG_ICON.png','/HBG_ICON.png','/BG_ICON.png');
+weapons = [
+    ['/GS_ICON.png', 'Great-Sword'],
+    ['/LS_ICON.png', 'Long-Sword'],
+    ['/SNS_ICON.png', 'Sword and Shield'],
+    ['/DB_ICON.png', 'Dual-Blades'],
+    ['/HA_ICON.png', 'Hammer'],
+    ['/HH_ICON.png', 'Hunting Horn'],
+    ['/LZ_ICON.png', 'Lance'],
+    ['/GLZ_ICON.png', 'Gun-Lance'],
+    ['/MA_ICON.png', 'Morph-Axe'],
+    ['/CB_ICON.png', 'Charge-Blade'],
+    ['/GLV_ICON.png', 'Insect-Glaive'],
+    ['/LBG_ICON.png', 'Light-Bowgun'],
+    ['/HBG_ICON.png', 'Heavy-Bowgun'],
+    ['/BG_ICON.png', 'Bow']
+];
 
-weaponNames.push('Great-Sword', 'Long-Sword', 'Sword and Shield', 'Dual-Blades', 'Hammer', 'Hunting Horn', 'Lance', 'Gun-Lance', 'Morph-Axe', 'Charge-Blade', 'Insect-Glaive', 'Light-Bowgun', 'Heavy-Bowgun', 'Bow');
+//weapons.push('/GS_ICON.png', '/LS_ICON.png', '/SNS_ICON.png', '/DB_ICON.png', '/HA_ICON.png', '/HH_ICON.png','/LZ_ICON.png','/GLZ_ICON.png','/MA_ICON.png','/CB_ICON.png','/GLV_ICON.png','/LBG_ICON.png','/HBG_ICON.png','/BG_ICON.png');
+
+//weaponNames.push('Great-Sword', 'Long-Sword', 'Sword and Shield', 'Dual-Blades', 'Hammer', 'Hunting Horn', 'Lance', 'Gun-Lance', 'Morph-Axe', 'Charge-Blade', 'Insect-Glaive', 'Light-Bowgun', 'Heavy-Bowgun', 'Bow');
 
 console.log(weapons.length+" Weapons found");
 
 let randomWeaponInt = getRandomInt(weapons.length-1);
-let targetWeapon = directory+weapons[randomWeaponInt];
+let targetWeapon = directory+weapons[randomWeaponInt][0];
 let targetElement = '';
-let targetWeaponName = weaponNames[randomWeaponInt];
+let targetWeaponName = weaponNames[randomWeaponInt][1];
 
 // Zugriff auf das Bild-Element über die ID 'img'
 const weaponElement = document.getElementById('Weapon');
@@ -31,8 +48,8 @@ imgElement.src = targetWeapon;
 
 function changeImage() {
     randomWeaponInt = getRandomInt(weapons.length-1);
-    targetWeapon = directory+weapons[randomWeaponInt];
-    targetWeaponName = weaponNames[randomWeaponInt];
+    targetWeapon = directory+weapons[randomWeaponInt][0];
+    targetWeaponName = weaponNames[randomWeaponInt][1];
 
     let hasElement = hasElementalDamage();
 
